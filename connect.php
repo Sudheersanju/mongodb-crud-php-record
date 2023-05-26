26d2381f38ff70009b6c759725943cff928b57b7
root <root@ip-172-31-31-213.us-west-2.compute.internal>
2023-05-26 12:08:38 +0000

    php-mngodb-crud-record

---
+++ connect.php
@@ -0,0 +1,33 @@
+<?php
+// Include Composer autoloader
+require 'vendor/autoload.php';
+
+// Define an array of Route53 record names
+$recordNames = [
+    'mongo-0.database.dawrinbox.local',
+    'mongo-1.database.dawrinbox.local',
+    'mongo-2.database.dawrinbox.local'
+];
+
+// Connect to MongoDB for each record name
+foreach ($recordNames as $recordName) {
+    // Extract the hostname from the record name
+    $hostname = explode('.', $recordName)[0];
+
+    // Connect to MongoDB with the hostname
+    $client = new MongoClient("mongodb://$hostname:27017");
+    echo "Connection to $recordName successful<br>";
+
+    // Select a database
+    $database = $client->nameDatabase;
+    echo "Database nameDatabase Selected for $recordName<br>";
+
+    // Create a collection
+    $collection = $database->createCollection("nameDatabase");
+    echo "Created collection successfully for $recordName<br>";
+
+    // Close the MongoDB connection
+    $client->close();
+}
+?>
+
